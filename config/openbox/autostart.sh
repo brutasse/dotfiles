e2670e3829838426af0fcd2b302b13fcbe30598f
Bruno Renié <brutasse@gmail.com>
2014-12-01 13:57:17 +0100

    Update autostart

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -2,11 +2,14 @@
 xrandr --dpi 110
 
 # Keyboard
-setxkbmap -model macintosh ch fr &
-xbindkeys &
+setxkbmap -model macintosh ch fr
+xbindkeys
 
 # SSH agent
-eval `ssh-agent` &
+eval `ssh-agent`
+
+# GPG agent
+eval `gpg-agent --daemon`
 
 eval `gnome-keyring-daemon`
 
@@ -15,9 +18,6 @@ amixer set Master 80% unmute
 amixer set Headphone 80% unmute
 amixer set Speaker 80% unmute
 amixer set PCM 80% unmute
-
-# Disable touchpad while typing
-syndaemon -t -i 1 -d &
 
 # Blank screen after 5 minutes
 xset dpms 300
@@ -41,17 +41,4 @@ cd ~
 
 xflux -l 7 &
 
-kupfer &
-
-# Make GTK apps look and behave how they were set up in the gnome config tools
-if test -x /usr/libexec/gnome-settings-daemon >/dev/null; then
-  /usr/libexec/gnome-settings-daemon &
-elif which gnome-settings-daemon >/dev/null 2>&1; then
-  gnome-settings-daemon &
-
-# Run XDG autostart things.  By default don't run anything desktop-specific
-# See xdg-autostart --help more info
-DESKTOP_ENV=""
-if which /usr/lib/openbox/xdg-autostart >/dev/null 2>&1; then
-  /usr/lib/openbox/xdg-autostart $DESKTOP_ENV
-fi
+kupfer --no-splash &
